10a11b7545be279ff1a2f31c6f8f8a7f3a99b617
Raven <Raven@DESKTOP-TUST0H3.(none)>
2023-04-15 12:05:12 +0300

    исправил

--- HW1/HomeWork3.py
+++ HW1/HomeWork3.py
@@ -3,8 +3,8 @@
 # где сумма первых трех цифр равна сумме последних трех. Т.е. билет с номером 385916 – счастливый, т.к. 3+8+5=9+1+6.
 # Вам требуется написать программу, которая проверяет счастливость билета.
 
-number = int(input('Введите номер билета'))
-res = list(map(int, str(number)))
+number = input('Введите номер билета')
+res = list(map(int, number))
 result1 = int(res[0]) + int(res[1]) + int(res[2])
 result2 = int(res[3]) + int(res[4]) + int(res[5])
 if result1 == result2:
